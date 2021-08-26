d9f91a69d3724624eb6cbd3d9668abdd8ab31cbd
golo <avgolosinda@gmail.com>
2021-08-26 13:47:34 +0800

    Create unregisterSymbolicLink.ps1

---
+++ unregisterSymbolicLink.ps1
@@ -0,0 +1,8 @@
+$AOSMetadataPath = "C:\AOSService\PackagesLocalDirectory"
+$RepoPath = "."
+$RepoMetadataPath = $RepoPath + "\Metadata"
+$RepoModelFolders = Get-ChildItem $RepoMetadataPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	cmd /c rmdir "$AOSMetadataPath\$ModelFolder"
+}
