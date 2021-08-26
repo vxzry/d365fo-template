a87825ad03c9c7a205227caebca4efce1a12ae0d
golo <avgolosinda@gmail.com>
2021-08-26 13:47:15 +0800

    Create registerSymbolicLink.ps1

---
+++ registerSymbolicLink.ps1
@@ -0,0 +1,9 @@
+$AOSMetadataPath = "C:\AOSService\PackagesLocalDirectory"
+$RepoPath = "."
+$RepoMetadataPath = $RepoPath + "\Metadata"
+$RepoModelFolders = Get-ChildItem $RepoMetadataPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	$Target = "$RepoMetadataPath\$ModelFolder"
+	New-Item -ItemType SymbolicLink -Path "$AOSMetadataPath" -Name "$ModelFolder" -Value "$Target"
+}
